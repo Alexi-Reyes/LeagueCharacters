3edbc17ba4c835aa58fcaaaf675a98bef1b54261
Alexi Reyes <alexireyes6796@gmail.com>
2024-02-25 15:56:08 +0100

    added get champion

--- backend/controllers/ChampionsController.js
+++ backend/controllers/ChampionsController.js
@@ -13,9 +13,11 @@ const createChampion = (req, res) => {
     })
     .then((champion) => {
         res.json(champion)
+        return
     })
     .then((error) => {
         res.json(error)
+        return
     })
 }
 
@@ -23,14 +25,30 @@ const getChampions = (req, res) => {
     prisma.champion.findMany()
     .then((champion) => {
         res.json(champion)
+        return
     })
     .then((error) => {
         res.json(error)
+        return
     })
 }
 
 const getChampion = (req, res) => {
-    //
+    let id = Number(req.params.id)
+
+    prisma.champion.findUnique({
+        where: {
+            id: id
+        }
+    })
+    .then((champion) => {
+        res.json(champion)
+        return
+    })
+    .then((error) => {
+        res.json(error)
+        return
+    })
 }
 
 const updateChampion = (req, res) => {
